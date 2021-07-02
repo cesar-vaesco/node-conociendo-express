a38de2051100270d29612e720926d14081e41d54
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-07-02 08:59:26 -0500

    Creando un servidor con node.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.status = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello world');
+});
+
+server.listen(3000, () => {
+    console.log('Server on port 3000')
+
+})
